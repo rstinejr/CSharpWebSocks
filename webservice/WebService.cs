765b9ddc753a5cafb33de00ea6e545741e66b5e1
R.H. Stine <bob@waltonstine.net>
2017-10-08 10:09:12 -0400

    Got MapGet() to work for handling request to "hello".

---
+++ webservice/WebService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace waltonstine.demo.csharp.webservice
+{
+  
+    public class WebService
+    {
+
+        private static IConfigurationRoot conf;
+
+        public static IConfigurationRoot Conf {
+            get { return conf; }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Start web server, current dir is {Directory.GetCurrentDirectory()}");
+
+            IConfigurationBuilder confBldr =
+               new ConfigurationBuilder();
+
+            conf = confBldr.Build();
+
+            IWebHost host =
+                new WebHostBuilder()
+                 //.ConfigureServices(services => { services.AddSingleton<IEvidenceUploadManager, EvidenceUploadManager>(); })
+                //.ConfigureServices(services => { services.AddSingleton<ISearchIdFactory,       SearchIdFactory>();   })
+                 .UseKestrel()
+                 .UseContentRoot(Directory.GetCurrentDirectory())
+                 .UseUrls("http://*:54321")
+                 .UseStartup<WebServiceStartup>()
+                 .Build();
+
+            host.Run();
+
+            // No return; host.Run() kicks off the Kestrel web server.
+            // Exit with ^C.
+        }
+    }
+}
--- webservice/WebServiceStartup.cs
+++ webservice/WebServiceStartup.cs
@@ -1,57 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
-namespace waltonstine.dotnetdemo.CSharpWebSocks.webservice
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace waltonstine.demo.csharp.webservice
 {
-    public class WebServiceStartup
+    /**
+     * Startup claass used by .NET Core Web Service. Defines the "Routes", which is ASP-speak for the local part
+     * of URI paths.
+     * 
+     * The ConfigureServices method enables logging and "routing" - URL handling.
+     * 
+     * The Configure method is used to define "routes" and associate URLs with handlers.
+     */
+    class WebServiceStartup
     {
-        public WebServiceStartup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
+        #region .NET Callbacks
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        /*
+         * ConfigureServices: called by the .NET Core web framework.
+         * 
+         * Denendency injection is used to add functionality to the web server.
+         * "routing" is used to associate local URI paths with handler routines. 
+         */
+        public void ConfigureServices(IServiceCollection services) {
             services
                 .AddRouting()
                 .AddLogging();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
+        /*
+         * Configure: called by the .NET Core web framework.
+         *
+         * In .NET Core, the Configure method is primarily used to create routes and
+         * associate them with handlers.
+         * 
+         * DEVLAN also uses this method for setting up the Controller.
+         */
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory) {
+
+            //loggerFactory.AddConsole();
+
+            log = loggerFactory
+                    .CreateLogger("CSharpWebSock");
 
             app
-                .UseDeveloperExceptionPage()
+                //.UseDeveloperExceptionPage()   //.UseExceptionHandler("/error");
                 .UseRouter(BuildRoutes(app));
         }
 
+
+        #endregion
+
+        #region Route Handlers
+
+        ////////////////////////////////////////////////////////////////////////////////////
+        /// Methods that respond to HTTP Requests
+        ////////////////////////////////////////////////////////////////////////////////////
+
+
+        /*
+         * Handle "GET /hello"
+         */
         private Task GotHello(HttpContext httpCtx)
         {
-            return httpCtx.Response.WriteAsync("hello");
+
+            return httpCtx.Response.WriteAsync("Hello, World!");
         }
 
-        private IRouter BuildRoutes(IApplicationBuilder app)
-        {
+
+        /*
+         * Dummy request handler to use until real guts can be put together.
+         * It echos the request, to indicate that the HTTP part of the server is hooked up.
+         */
+        private Task EchoRequest(HttpContext httpCtx) {
+            if (httpCtx.Request.Method == "POST") {
+                StreamReader rdr = new StreamReader(httpCtx.Request.Body);
+                string body = rdr.ReadToEnd();
+                log.LogDebug($"Body of POST: {body}");
+            }
+
+            return httpCtx.Response.WriteAsync(httpCtx.Request.Path.ToString());
+        }
+
+        #endregion
+
+
+        private ILogger            log;
+
+
+        #region Routing
+
+        /*
+         * Set up URL handling for the Controller API.
+         * URLs are listed above, in the initial file header.
+         */
+        private IRouter BuildRoutes(IApplicationBuilder app) {
+
             RouteBuilder routeBuilder = new RouteBuilder(app, new RouteHandler(null));
+
 
             routeBuilder.MapGet("hello", GotHello);
 
             return routeBuilder.Build();
         }
+
+        #endregion
 
     }
 }
